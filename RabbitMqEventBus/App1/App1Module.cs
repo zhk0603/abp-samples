db342c864e2ff6421b6045f7dd25a17cf2968007
赵坤 <zk9474@hotmail.com>
2020-05-25 09:10:36 +0800

    aa

--- RabbitMqEventBus/App1/App1Module.cs
+++ RabbitMqEventBus/App1/App1Module.cs
@@ -1,6 +1,7 @@
 ﻿using Volo.Abp.Autofac;
 using Volo.Abp.EventBus.RabbitMq;
 using Volo.Abp.Modularity;
+using Volo.Abp.RabbitMQ;
 
 namespace App1
 {
@@ -12,6 +13,13 @@ namespace App1
     {
         public override void ConfigureServices(ServiceConfigurationContext context)
         {
+            Configure<AbpRabbitMqOptions>(options =>
+            {
+                options.Connections.Default.UserName = "guest";
+                options.Connections.Default.Password = "guest";
+                options.Connections.Default.HostName = "192.168.1.11";
+            });
+
             Configure<AbpRabbitMqEventBusOptions>(options =>
             {
                 options.ClientName = "TestApp1";
